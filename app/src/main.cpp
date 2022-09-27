3a3403088451c7af9bb39943327d9fa6bd162981
kylegranger <kyle.granger@chello.at>
2022-09-27 16:18:23 +0200

    basically works

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -10,30 +10,16 @@
 
 namespace plc::app {
 
-// TODO: use proper logger config, this one was just copy-pasted
-static const std::string logger_config = R"(
-# ----------------
-sinks:
-  - name: console
-    type: console
-    color: true
-groups:
-  - name: main
-    sink: console
-    level: info
-    children:
-      - name: libp2p
-# ----------------
-  )";
-
 void prepareLogging() {
-    // prepare log system
-    auto logging_system = std::make_shared<soralog::LoggingSystem>(
-        std::make_shared<soralog::ConfiguratorFromYAML>(
-            // Original LibP2P logging config
-            std::make_shared<libp2p::log::Configurator>(),
-            // Additional logging config for application
-            logger_config));
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+    std::shared_ptr<soralog::Configurator> yaml_configurator_from_file =
+    std::make_shared<soralog::ConfiguratorFromYAML>(
+        std::filesystem::path("./config/logger.yml"));
+
+    std::shared_ptr<soralog::Configurator> configurator = yaml_configurator_from_file;
+
+    auto logging_system = std::make_shared<soralog::LoggingSystem>(configurator);
+
     auto r = logging_system->configure();
     if (!r.message.empty()) {
         (r.has_error ? std::cerr : std::cout) << r.message << std::endl;
@@ -45,6 +31,8 @@ void prepareLogging() {
     libp2p::log::setLoggingSystem(logging_system);
     if (std::getenv("TRACE_DEBUG") != nullptr) {
         libp2p::log::setLevelOfGroup("main", soralog::Level::TRACE);
+    } else if (std::getenv("INFO_DEBUG") != nullptr)  {
+        libp2p::log::setLevelOfGroup("main", soralog::Level::INFO);
     } else {
         libp2p::log::setLevelOfGroup("main", soralog::Level::ERROR);
     }
@@ -59,6 +47,15 @@ int main(const int count, const char** args) {
     using namespace plc::core;
 
     prepareLogging();
+
+    auto log_ = libp2p::log::createLogger("main");
+    log_->trace("Example of trace log message");
+    log_->debug("There is a debug value in this line: {}", 0xDEADBEEF);
+    log_->verbose("Let's gossip about something");
+    log_->info("This is simple info message");
+    log_->warn("This is formatted message with level '{}'", "warning");
+    log_->error("This is message with level '{}' and number {}", "error", 777);
+    log_->critical("This is example of critical situations");
 
     auto runner = runner::ClientRunner();
 
