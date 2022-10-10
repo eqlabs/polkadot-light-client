7b32c938ebf2e01b9de64164c943401c53dda138
kylegranger <kyle.granger@chello.at>
2022-10-10 16:55:22 +0200

    add github host to cmake command

--- cmake/Conan/cpp-libp2p.cmake
+++ cmake/Conan/cpp-libp2p.cmake
@@ -3,9 +3,11 @@
 set(CPP_LIBP2P_ROOT "${CMAKE_BINARY_DIR}/cpp-libp2p.cmake")
 file(MAKE_DIRECTORY "${CPP_LIBP2P_ROOT}")
 
+set(LIBP2P_GITHUB_HOST "github.com" CACHE STRING "Host for cpp-libp2p repo (maybe overriden to provide custom ssh key)")
+
 # Configure external cpp-libp2p
 execute_process(
-    COMMAND ${CMAKE_COMMAND} "${CMAKE_CURRENT_LIST_DIR}/cpp-libp2p" -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} -DSAN=${SAN}
+    COMMAND ${CMAKE_COMMAND} "${CMAKE_CURRENT_LIST_DIR}/cpp-libp2p" -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} -DSAN=${SAN} -DGITHUB_HOST=${LIBP2P_GITHUB_HOST}
     WORKING_DIRECTORY "${CPP_LIBP2P_ROOT}"
 )
 
