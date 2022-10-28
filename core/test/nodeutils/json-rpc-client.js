717af18c6fa75d4a6c9d8214a4367d8d2fd393b4
kylegranger <kyle.granger@chello.at>
2022-10-28 09:07:52 +0200

    swap ws for websocket module; explicitly close connection

--- core/test/nodeutils/json-rpc-client.js
+++ core/test/nodeutils/json-rpc-client.js
@@ -1,33 +1,12 @@
-const { exit } = require('process');
+var WebSocket = require('ws');
 
-var WebSocketClient = require('websocket').client;
+const ws = new WebSocket('ws://127.0.0.1:2584');
 
-var client = new WebSocketClient();
-
-client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
+ws.on('open', function open() {
+  ws.send(process.argv[2]);
 });
 
-client.on('connect', function(connection) {
-    console.log('WebSocket Client Connected');
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
-    });
-    connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
-    });
-    connection.on('message', function(message) {
-        console.log('got message', message)
-        exit();
-    });
-
-    function sendMessage() {
-        if (connection.connected) {
-            console.log('sending message: ', process.argv[2])
-            connection.send(process.argv[2]);
-        }
-    }
-    sendMessage();
+ws.on('message', function message(data) {
+  console.log('received: %s', data);
+  ws.close()
 });
-
-client.connect('ws://127.0.0.1:2584');
